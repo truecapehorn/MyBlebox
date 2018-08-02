2f73dbc25c35ec45c52e8c2edf3b2a8ce6a71f52
truecapehorn <truecapehorn@gmail.com>
2018-08-02 14:56:53 +0200

    pierwsza wersja programu wykonawczego

--- agrp_sample.py
+++ agrp_sample.py
@@ -1,5 +1,4 @@
-from switchBoxD_API import Blebox
-import time
+
 import argparse
 import textwrap
 
@@ -11,7 +10,6 @@ def str2bool(v):
         return 0
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
-
 
 parser = argparse.ArgumentParser(
     prog='PROG',
@@ -38,6 +36,7 @@ parser.add_argument('-w', action='store', dest='action_w', type=str2bool,
 parser.add_argument('-a', action='store', dest='action_a', type=str2bool,
                     help='Sterowanie zbiorcze')
 
+
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
 results = parser.parse_args()  # pobranie rezultatow parsera
@@ -49,53 +48,31 @@ action_b = results.action_b
 action_p = results.action_p
 action_w = results.action_w
 action_a = results.action_a
-action = [action_hl, action_hp, action_ln, action_b, action_p, action_w, action_a]
+action=[action_hl,action_hp,action_ln,action_b,action_p,action_w,action_a]
 
 ip_halospoty = '192.168.1.201'
 ip_lampki = '192.168.1.202'
 ip_kotlownia = '192.168.1.203'
 ips = [ip_halospoty, ip_lampki, ip_kotlownia]
 
-halospoty = Blebox(ip_halospoty)
-lampki = Blebox(ip_lampki)
-kotlownia = Blebox(ip_kotlownia)
-devs = [halospoty, lampki, kotlownia]
-
-
 for i in action:
     print(i)
 
-if action_hl != None:
-    print('halospoty lewe akcja', action_hl)
-    halospoty.relay_set_get(0, action_hl)
-if action_hp != None:
-    print('halospoty prawe akcja', action_hp)
-    halospoty.relay_set_get(1, action_hp)
-if action_ln != None:
-    print('lampka nocna akcja', action_ln)
-    lampki.relay_set_get(0, action_ln)
-if action_b != None:
-    print('biurko akcja', action_b)
-    lampki.relay_set_get(1, action_b)
-if action_p != None:
-    print('piecyk akcja', action_p)
-    kotlownia.relay_set_get(0, action_p)
-if action_w != None:
-    print('wiatrak akcja', action_w)
-    kotlownia.relay_set_get(1, action_w)
-if action_a != None:
-    print('akcja grupowa', action_a)
-    for dev in devs:
-        for relay in [0, 1]:
-            print(dev.device_adress, relay, action_a)
-            dev.relay_set_get(relay, action_a)
-            time.sleep(1)
+if action_hl!=None:
+    print( 'halospoty lewe akcja',action_hl)
+if action_hp!=None:
+    print( 'halospoty prawe akcja',action_hp)
+if action_ln!=None:
+    print( 'lampka nocna akcja',action_ln)
+if action_b!=None:
+    print( 'biurko akcja',action_b)
+if action_p!=None:
+    print( 'piecyk akcja',action_p)
+if action_w!=None:
+    print( 'wiatrak akcja',action_w)
+if action_a!=None:
+    print( 'gupa akcja',action_a)
 
-if __name__ == '__main__':
-    for i in devs:
-        blebox = i
-        print(blebox.relay_state())
-        time.sleep(1)
-    lampki.relay_set_get(1, 1)
-    time.sleep(5)
-    lampki.relay_set_get(1, 0)
+
+
+
