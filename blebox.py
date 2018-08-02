9adf2cffb8318cf714176efa7248f64f79024615
truecapehorn <truecapehorn@gmail.com>
2018-08-02 16:51:48 +0200

    program po dopracowaniu

--- blebox.py
+++ blebox.py
@@ -4,6 +4,17 @@ import argparse
 import textwrap
 
 
+ip_halospoty = '192.168.1.201'
+ip_lampki = '192.168.1.202'
+ip_kotlownia = '192.168.1.203'
+ips = [ip_halospoty, ip_lampki, ip_kotlownia]
+
+halospoty = Blebox(ip_halospoty)
+lampki = Blebox(ip_lampki)
+kotlownia = Blebox(ip_kotlownia)
+devs = [halospoty, lampki, kotlownia]
+devs_lamp = [halospoty, lampki]
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 'h', 'y', '1'):
         return 1
@@ -17,9 +28,11 @@ parser = argparse.ArgumentParser(
     prog='PROG',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     description=textwrap.dedent('''\
-
         --------------------------------------------------------
-
+        Uruchomienie bleboxow
+        Dozwolone wartosci dla argumentow to:
+            ('yes', 'true', 'h', 'y', '1')
+            ('no', 'false', 'l', 'n', '0')
         --------------------------------------------------------
         '''))
 
@@ -27,16 +40,21 @@ parser.add_argument('-hl', action='store', dest='action_hl', type=str2bool,
                     help='Sterowanie halospoty lewe')
 parser.add_argument('-hp', action='store', dest='action_hp', type=str2bool,
                     help='Sterowanie halospoty prawe')
-parser.add_argument('-ln', action='store', dest='action_ln', type=str2bool,
+parser.add_argument('-l', action='store', dest='action_l', type=str2bool,
                     help='Sterowanie lampka nocna')
 parser.add_argument('-b', action='store', dest='action_b', type=str2bool,
                     help='Sterowanie biurko')
 parser.add_argument('-p', action='store', dest='action_p', type=str2bool,
                     help='Sterowanie piecyk')
 parser.add_argument('-w', action='store', dest='action_w', type=str2bool,
-                    help='Sterowanie piecyk')
-parser.add_argument('-a', action='store', dest='action_a', type=str2bool,
-                    help='Sterowanie zbiorcze')
+                    help='Sterowanie wiatrak')
+parser.add_argument('-a', action='store', dest='action_lamp', type=str2bool,
+                    help='Sterowanie zbiorcze dla oswietenia')
+parser.add_argument('-A', action='store', dest='action_all', type=str2bool,
+                    help='Sterowanie zbiorcze wszystko')
+parser.add_argument('--state', action='store_true', default=False,
+                    dest='action_state',
+                    help='Sprawdzenie stanow')
 
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
@@ -44,58 +62,51 @@ results = parser.parse_args()  # pobranie rezultatow parsera
 
 action_hl = results.action_hl
 action_hp = results.action_hp
-action_ln = results.action_ln
+action_l = results.action_l
 action_b = results.action_b
 action_p = results.action_p
 action_w = results.action_w
-action_a = results.action_a
-action = [action_hl, action_hp, action_ln, action_b, action_p, action_w, action_a]
-
-ip_halospoty = '192.168.1.201'
-ip_lampki = '192.168.1.202'
-ip_kotlownia = '192.168.1.203'
-ips = [ip_halospoty, ip_lampki, ip_kotlownia]
-
-halospoty = Blebox(ip_halospoty)
-lampki = Blebox(ip_lampki)
-kotlownia = Blebox(ip_kotlownia)
-devs = [halospoty, lampki, kotlownia]
-
-
-for i in action:
-    print(i)
+action_lamp = results.action_lamp
+action_all = results.action_all
+action_state=results.action_state
+action = [action_hl, action_hp, action_l, action_b, action_p, action_w, action_lamp, action_all]
 
 if action_hl != None:
-    print('halospoty lewe akcja', action_hl)
-    halospoty.relay_set_get(0, action_hl)
+    print('akcja dla halospoty lewe', action_hl)
+    print(halospoty.relay_set_get(0, action_hl))
 if action_hp != None:
-    print('halospoty prawe akcja', action_hp)
-    halospoty.relay_set_get(1, action_hp)
-if action_ln != None:
-    print('lampka nocna akcja', action_ln)
-    lampki.relay_set_get(0, action_ln)
+    print('akcja dla halospoty prawe', action_hp)
+    print(halospoty.relay_set_get(1, action_hp))
+if action_l != None:
+    print('akcja dla lampki nocnej', action_l)
+    print(lampki.relay_set_get(0, action_l))
 if action_b != None:
-    print('biurko akcja', action_b)
-    lampki.relay_set_get(1, action_b)
+    print('akcja dla biurka', action_b)
+    print(lampki.relay_set_get(1, action_b))
 if action_p != None:
-    print('piecyk akcja', action_p)
-    kotlownia.relay_set_get(0, action_p)
+    print('akcja dla piecyka', action_p)
+    print(kotlownia.relay_set_get(0, action_p))
 if action_w != None:
-    print('wiatrak akcja', action_w)
-    kotlownia.relay_set_get(1, action_w)
-if action_a != None:
-    print('akcja grupowa', action_a)
+    print('akcja dla wiatrak', action_w)
+    print(kotlownia.relay_set_get(1, action_w))
+if action_lamp != None:
+    print('akcja grupowa', action_lamp)
+    for dev in devs_lamp:
+        for relay in [0, 1]:
+            dev.relay_set_get(relay, action_lamp)
+            time.sleep(0.5)
+        print(dev.relay_state())
+if action_all != None:
+    print('akcja grupowa', action_all)
     for dev in devs:
         for relay in [0, 1]:
-            print(dev.device_adress, relay, action_a)
-            dev.relay_set_get(relay, action_a)
-            time.sleep(1)
+            dev.relay_set_get(relay, action_all)
+            time.sleep(0.5)
+        print(dev.relay_state())
+if action_state == True:
+    print('sprawdzenie stanow', action_state)
+    for dev in devs:
+        print(dev.relay_state())
+        time.sleep(0.5)
 
-if __name__ == '__main__':
-    for i in devs:
-        blebox = i
-        print(blebox.relay_state())
-        time.sleep(1)
-    lampki.relay_set_get(1, 1)
-    time.sleep(5)
-    lampki.relay_set_get(1, 0)
+
