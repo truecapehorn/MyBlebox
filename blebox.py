64551f03c37db8f142dd93a19a62b35060a7cad5
truecapehorn <truecapehorn@gmail.com>
2018-09-23 10:45:31 +0200

    1. Dodanie nowych bleboxow 2. Dodanie excpetion dla funkcji drukowania. Teraz tylko wyswietla blad zamiast wychodzic z programu

--- blebox.py
+++ blebox.py
@@ -6,12 +6,19 @@ import textwrap
 ip_halospoty = '192.168.1.201'
 ip_lampki = '192.168.1.202'
 ip_kotlownia = '192.168.1.203'
+ip_kuchnia = "192.168.1.204"
+ip_wejscie = "192.168.1.205"
+
 ips = [ip_halospoty, ip_lampki, ip_kotlownia]
 
+#   dodanie nowych urzadzen
 halospoty = SwichBoxD(ip_halospoty)
 lampki = SwichBoxD(ip_lampki)
 kotlownia = SwichBoxD(ip_kotlownia)
-swBox = [halospoty, lampki, kotlownia]
+kuchnia = SwichBoxD(ip_kuchnia)
+wejscie = SwichBoxD(ip_wejscie)
+
+swBox = [halospoty, lampki, kotlownia, kuchnia, wejscie]
 devs_lamp = [halospoty, lampki]
 
 
@@ -25,7 +32,10 @@ def str2bool(v):
 
 
 def relay_out(r, nr_relay):
-    return r["relays"][nr_relay]["name"], r["relays"][nr_relay]["state"]
+    try:
+        return r["relays"][nr_relay]["name"], r["relays"][nr_relay]["state"]
+    except Exception as e:
+        print(e)
 
 
 parser = argparse.ArgumentParser(
@@ -52,6 +62,15 @@ parser.add_argument('-p', action='store', dest='action_p', type=str2bool,
                     help='Sterowanie piecyk')
 parser.add_argument('-w', action='store', dest='action_w', type=str2bool,
                     help='Sterowanie wiatrak')
+parser.add_argument('-k', action='store', dest='action_k', type=str2bool,
+                    help='Sterowanie kuchnia')
+parser.add_argument('-mp', action='store', dest='action_mp', type=str2bool,
+                    help='Sterowanie mały pokój')
+parser.add_argument('-we', action='store', dest='action_we', type=str2bool,
+                    help='Sterowanie wejście')
+parser.add_argument('-laz', action='store', dest='action_laz', type=str2bool,
+                    help='Sterowanie łazienka')
+
 parser.add_argument('-a', action='store', dest='action_lamp', type=str2bool,
                     help='Sterowanie zbiorcze dla oswietenia')
 parser.add_argument('-A', action='store', dest='action_all', type=str2bool,
@@ -60,22 +79,26 @@ parser.add_argument('--state', action='store_true', default=False,
                     dest='action_state',
                     help='Sprawdzenie stanow')
 
-parser.add_argument('--version', action='version', version='%(prog)s 1.1')
+parser.add_argument('--version', action='version', version='%(prog)s 1.2')
 
 results = parser.parse_args()  # pobranie rezultatow parsera
 
+# odczyt akacji
 action_hl = results.action_hl
 action_hp = results.action_hp
 action_l = results.action_l
 action_b = results.action_b
 action_p = results.action_p
 action_w = results.action_w
+action_k = results.action_k
+action_mp = results.action_mp
+action_we = results.action_we
+action_laz = results.action_laz
+
 action_lamp = results.action_lamp
 action_all = results.action_all
 action_state = results.action_state
 actions = [action_hl, action_hp, action_l, action_b, action_p, action_w, action_lamp, action_all]
-
-
 
 if action_hl != None:
     r = halospoty.relay_set_get(0, action_hl)
@@ -83,24 +106,41 @@ if action_hl != None:
 if action_hp != None:
     r = halospoty.relay_set_get(1, action_hp)
     print(relay_out(r, 1))
+
 if action_l != None:
     r = lampki.relay_set_get(0, action_l)
     print(relay_out(r, 0))
 if action_b != None:
     r = lampki.relay_set_get(1, action_b)
     print(relay_out(r, 1))
+
 if action_p != None:
     r = kotlownia.relay_set_get(0, action_p)
     print(relay_out(r, 0))
 if action_w != None:
     r = kotlownia.relay_set_get(1, action_w)
     print(relay_out(r, 1))
+
+if action_k != None:
+    r = kuchnia.relay_set_get(0, action_k)
+    print(relay_out(r, 0))
+if action_mp != None:
+    r = kuchnia.relay_set_get(1, action_mp)
+    print(relay_out(r, 1))
+
+if action_we != None:
+    r = wejscie.relay_set_get(0, action_we)
+    print(relay_out(r, 0))
+if action_laz != None:
+    r = kuchnia.relay_set_get(1, action_laz)
+    print(relay_out(r, 1))
+
 if action_lamp != None:
     print('akcja grupowa', action_lamp)
     for dev in devs_lamp:
         for relay in [0, 1]:
             dev.relay_set_get(relay, action_lamp)
-            #time.sleep(0.1)
+            # time.sleep(0.1)
             r = dev.relay_state()
             print(relay_out(r, relay))
 if action_all != None:
@@ -108,7 +148,7 @@ if action_all != None:
     for dev in devs:
         for relay in [0, 1]:
             dev.relay_set_get(relay, action_all)
-            #time.sleep(0.1)
+            # time.sleep(0.1)
             r = dev.relay_state()
             print(relay_out(r, relay))
 if action_state == True:
@@ -123,4 +163,3 @@ if action_state == True:
         print("{}: {} ".format("Switch State", box.switch_state()))
         print("{}: {} ".format("Dev State", box.device_state()))
         print("{}: {} ".format("Up Time", box.devive_uptime()))
-
