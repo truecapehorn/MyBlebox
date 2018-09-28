f05702760c32cbb8b534e2618726c4bece371eee
truecapehorn <truecapehorn@gmail.com>
2018-09-28 21:27:52 +0200

    ostatnie poprawki

--- blebox.py
+++ blebox.py
@@ -96,12 +96,13 @@ parser.add_argument('--version', action='version', version='%(prog)s 1.2')
 
 actions = vars(parser.parse_args())  # pobranie wartosci akcji z namespace parasera w postaci slownika
 
-# zadeklarowanie akcji
+# zadeklarowanie akcji grupowych
 actionAll = ["action_hl", "action_hp", "action_l", "action_b", "action_p", "action_w", "action_k", "action_mp",
              "action_we", "action_zw", ]
 actionLamps = ["action_hl", "action_hp", "action_l", "action_b", "action_k", "action_mp", "action_we", "action_zw", ]
 actionEnter = ["action_hl", "action_hp", "action_k ", "action_mp", "action_we", "action_zw", ]
 actionSalon = ["action_l", "action_b", ]
+
 
 if actions["action_lamp"] is not None:  # przypisanie akcji gupowej dla wszystkich lamp
     for i in actionLamps:
