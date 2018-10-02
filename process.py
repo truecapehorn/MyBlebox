8874583841d44259021afb76ffca258b8b75ecf3
truecapehorn <truecapehorn@gmail.com>
2018-10-02 19:37:16 +0200

    clasa dziala ale sprawdzam jak sie zachowa jak t dam do innit

--- process.py
+++ process.py
@@ -1,12 +1,28 @@
 from lib.blebox_API import SwichBoxD
 import time
+from threading import Timer
+
+class RepeatableTimer(object):
+    def __init__(self, interval, function, args=[], kwargs={}):
+        self._interval = interval
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+    def start(self):
+        self.t = Timer(self._interval, self._function, *self._args, **self._kwargs)
+        self.t.start()
+    def stop(self):
+        self.t.cancel()
+    def alive(self):
+        self.t.is_alive()
 
 def set_proc_name(newname):
     from ctypes import cdll, byref, create_string_buffer
     libc = cdll.LoadLibrary('libc.so.6')
-    buff = create_string_buffer(len(newname)+1)
+    buff = create_string_buffer(len(newname) + 1)
     buff.value = newname
     libc.prctl(15, byref(buff), 0, 0, 0)
+
 
 def get_proc_name():
     from ctypes import cdll, byref, create_string_buffer
@@ -15,6 +31,7 @@ def get_proc_name():
     # 16 == PR_GET_NAME from <linux/prctl.h>
     libc.prctl(16, byref(buff), 0, 0, 0)
     return buff.value
+
 
 set_proc_name(b'blebox_norbert')
 
@@ -37,46 +54,34 @@ wejscie = SwichBoxD(ip_wejscie)
 def check():
     hl = halospoty.relay_state()['relays'][0]['state']
     hp = halospoty.relay_state()['relays'][1]['state']
-    print(hl,hp)
+    print(hl, hp)
     time.sleep(2)
-    if hp==1 or hl==1:
+    if hp == 1 or hl == 1:
         return True
+    else:return False
 
 
-from threading import Thread
-from time import sleep
 
-seconds=30
+seconds = 5
+
+
 # Function to be called when the timer expires
-def myFunction():
-    halospoty.relay_set_get(1,0)
-    halospoty.relay_set_get(0,0)
+def halospotyOff():
+    halospoty.relay_set_get(1, 0)
+    halospoty.relay_set_get(0, 0)
 
-# Function with the timer
-def myTimer(seconds):
-    sleep(seconds)
-    myFunction()
 
-# Thread that will sleep in background and call your function
-# when the timer expires.
-myThread = Thread(target=myTimer, args=(4,))
-myThread.start()
+a=RepeatableTimer(5,halospotyOff)
+
 
 while True:
-    status=check()
-    print(status)
-    if status==True:
-        myThread = Thread(target=myTimer, args=(4,))
-        myThread.start()
+    print('status?', check())
+    print(a.alive())
+    if check()==True:
+        a.start()
+        print('a start')
 
-
-
-
-
-
-
-
-
-
-
-
+    print("Program glowny")
+    for i in range(1, 5):
+        print(i)
+        time.sleep(1)
---
+++ timer.py
@@ -0,0 +1,33 @@
+from threading import Timer
+import time
+
+class RepeatableTimer(object):
+    def __init__(self, interval, function, args=[], kwargs={}):
+        self._interval = interval
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+    def start(self):
+        self.t = Timer(self._interval, self._function, *self._args, **self._kwargs)
+        self.t.start()
+    def stop(self):
+        self.t.cancel()
+    def alive(self):
+        print(self.t.is_alive())
+
+
+def hello():
+    print ("hello")
+
+a=RepeatableTimer(3,hello)
+
+for i in range(1,5):
+    print(i)
+    a.start()
+    time.sleep(4)
+    if i == 3:
+        a.stop()
+    a.alive()
+
+
+
