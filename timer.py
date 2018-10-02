df1c9593386093efa18431da8746fa21c74cd1da
truecapehorn <truecapehorn@gmail.com>
2018-10-02 21:22:54 +0200

    komit poprzedni ale plik timer stary

--- timer.py
+++ timer.py
@@ -1,33 +1,42 @@
-from threading import Timer
-import time
 
-class RepeatableTimer(object):
-    def __init__(self, interval, function, args=[], kwargs={}):
-        self._interval = interval
-        self._function = function
-        self._args = args
-        self._kwargs = kwargs
-    def start(self):
-        self.t = Timer(self._interval, self._function, *self._args, **self._kwargs)
-        self.t.start()
-    def stop(self):
-        self.t.cancel()
-    def alive(self):
-        print(self.t.is_alive())
+from __future__ import print_function
+
+
+from threading import Timer
 
 
 def hello():
-    print ("hello")
-
-a=RepeatableTimer(3,hello)
-
-for i in range(1,5):
-    print(i)
-    a.start()
-    time.sleep(1)
-    if i == 3:
-        a.stop()
-    a.alive()
+    print("Hello World!")
 
 
+class RepeatingTimer(object):
 
+    def __init__(self, interval, f, *args, **kwargs):
+        self.interval = interval
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+
+        self.timer = None
+
+
+    def callback(self):
+        self.f(*self.args, **self.kwargs)
+        self.start()
+
+    def cancel(self):
+        self.timer.cancel()
+
+    def start(self):
+        self.timer = Timer(self.interval,self.f)
+        self.timer.start()
+    def run(self):
+        self.timer.run()
+
+
+
+t = RepeatingTimer(3, hello)
+print(t.run())
+t.start()
+print("d")
+print(t.run())
