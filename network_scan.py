346a3aefce9808c213d75e03ea05a1bcab3c01f4
truecapehorn <truecapehorn@gmail.com>
2020-01-13 21:21:15 +0100

    usprawnienia poprzednich

--- network_scan.py
+++ network_scan.py
@@ -12,11 +12,11 @@ def box_find(adres):
     sw=SwichBoxD(adres)
     state=sw.device_state()
     if type(state) is dict:
-        return {"adress":adres,'type':state['device']['type']}
+        return {"adress":adres,'type':state['device']['type'], "deviceName":state['device']['deviceName']}
 
 def check(adr):
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5000) as executor:
         try:
             print(f"Sprawdznie adresów")
             results = executor.map(box_find,adr)
@@ -30,7 +30,8 @@ def check(adr):
 
 if __name__=='__main__':
     # address = [f'192.168.1.{i}' for i in range(0, 250)]
-    address=[f"192.168.{x}.{y}" for x in range(0,10) for y in range(0,250)]
+    address=[f"192.168.{x}.{y}" for x in range(0,11) for y in range(0,250)]
+    print(len(address))
     lista = check(address)
     print(lista)
     exit(0)
